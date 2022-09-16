c1a8294de3d18be9ffc7215e049c0fca27932dcf
husanbek <husanbek4030m@gmail.com>
2022-09-16 21:37:51 +0500

    Initial Commit

---
+++ HFirePerfect/HFirePerfectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HFirePerfectApp.swift
+//  HFirePerfect
+//
+//  Created by Karavella on 16/09/22.
+//
+
+import SwiftUI
+
+@main
+struct HFirePerfectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
